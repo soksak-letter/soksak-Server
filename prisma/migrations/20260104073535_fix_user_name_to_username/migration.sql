0be6fd3da18187e9027ad27e2451cc4e02c9351b
pumisj <pumisj@naver.com>
2026-01-04 16:36:19 +0900

    fix: username 컬럼명 수정 (#16)i

---
+++ prisma/migrations/20260104073535_fix_user_name_to_username/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `user_name` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `user` DROP COLUMN `user_name`,
+    ADD COLUMN `username` VARCHAR(191) NULL;
