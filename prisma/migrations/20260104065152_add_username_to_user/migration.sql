484ca1e6b13cad6d36d0f57720ea463cfad1a82a
pumisj <pumisj@naver.com>
2026-01-06 16:53:20 +0900

    fix: DB 및 마이그레이션 내역 초기화

--- prisma/migrations/20260104065152_add_username_to_user/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `user_name` VARCHAR(191) NULL;
--- prisma/migrations/20260104121529_add_unique_attribute_to_user_id_in_auth/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[user_id]` on the table `auth` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `auth_user_id_key` ON `auth`(`user_id`);
