e9e9ff58e36e3e63a5c9eaaee0687f51dbb62b24
pumisj <pumisj@naver.com>
2026-01-15 21:50:11 +0900

    deploy: cicd 파이프라인 구성 (#41)

--- prisma/migrations/20260106101017_add_asset_url_to_letter_asset_font/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `asset_url` to the `letter_asset_font` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `letter_asset_font` ADD COLUMN `asset_url` VARCHAR(191) NOT NULL;
