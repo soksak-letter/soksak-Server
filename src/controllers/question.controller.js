63017e807b7c39c02eed79b0a3a6cd9a97e91094
pumisj <pumisj@naver.com>
2026-01-16 10:39:01 +0900

    feat: 오늘의 질문 조회 API 구현 (#44)

---
+++ src/controllers/question.controller.js
@@ -0,0 +1,11 @@
+import { getTodayQuestion } from "../services/question.service.js";
+
+export const handleGetTodayQuestion = async (req, res, next) => {
+    try{
+        const question = await getTodayQuestion();
+        
+        res.status(200).success( question );
+    } catch(err) {
+        next(err);
+    }
+}
