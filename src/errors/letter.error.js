6f70f81c454a078db1f1ef6751f35d7b2487e4d5
user040131 <seungjub270@gmail.com>
2026-01-21 02:47:36 +0900

    에러 파일 수정

--- src/errors/letter.error.js
+++ src/errors/letter.error.js
@@ -1,31 +1,21 @@
-
-export class BaseError extends Error {
-  constructor(message, status, errorCode, data = null) {
-    super(message);
-    this.status = status;
-    this.errorCode = errorCode;
-    this.reason = message;
-    this.data = data;
-  }
-}
-
+import { BadRequestError, NotFoundError } from "../errors/base.error.js";
 /**
  * 400: 자기 자신에게 친구 신청 (FRIEND_400_01)
  */
-export class SelfFriendRequestError extends BaseError {
+export class SelfFriendRequestError extends BadRequestError {
   constructor(message = "자기 자신과는 친구 신청을 할 수 없습니다.") {
     super(message, 400, "FRIEND_400_01", null);
   }
 }
 
-export class LetterBadRequest extends BaseError {
+export class LetterBadRequest extends BadRequestError {
     constructor(
     ) {
         super(this.message, 400, "LETTER_400_01", null);
     }
 }
 
-export class LetterNotFoundError extends BaseError {
+export class LetterNotFoundError extends NotFoundError {
     constructor(message="편지를 찾을 수 없습니다.") {
         super(message, 404, "LETTER_404_01", null);
     }
--- src/errors/report.error.js
+++ src/errors/report.error.js
@@ -1,34 +1,18 @@
-// errors/report.error.js
+import { BadRequestError, InternalServerError } from "./base.error.js"
 
-// 기본 에러 클래스
-export class BaseError extends Error {
-  constructor({
-    message,
-    status,
-    errorCode,
-    data = null,
-  }) {
-    super(message);
-    this.status = status;
-    this.errorCode = errorCode;
-    this.reason = message;
-    this.data = data;
-  }
-}
-
-export class ReportUnexpectedArgumentError extends BaseError {
+export class ReportUnexpectedArgumentError extends BadRequestError {
   constructor(message="잘못된 인자 입력입니다.") {
     super(message, 400, "REPORT_400_1", data)
   }
 }
 
-export class ReportInternalError extends BaseError {
+export class ReportInternalError extends BadRequestError {
   constructor(message="db 에러입니다.") {
     super(message, 500, "REPORT_500_1", data) 
   }
 }
 
-export class UnExpectedReportReasonError extends BaseError {
+export class UnExpectedReportReasonError extends InternalServerError {
   constructor(message="잘못된 report reason입니다.") {
     super(message, 400, "REPORT_400_02", data)
   }
