3b0d8a074e4803ecad396ca6f8b105612a36f560
pumisj <pumisj@naver.com>
2026-01-21 20:51:11 +0900

    fix: 편지 도메인 에러코드 수정 (#86)

--- src/errors/letter.error.js
+++ src/errors/letter.error.js
@@ -1,6 +1,12 @@
-import { BadRequestError } from "./base.error.js";
+import { BadRequestError, NotFoundError } from "./base.error.js";
 
 export class LetterBadRequest extends BadRequestError {
+    constructor(code, message, data = null) {
+        super(code, message, data);
+    }
+}
+
+export class LetterNotFound extends NotFoundError {
     constructor(code, message, data = null) {
         super(code, message, data);
     }
