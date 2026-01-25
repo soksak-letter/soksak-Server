00f386849c2de20227a2ce13a942080129b2d754
user040131 <seungjub270@gmail.com>
2026-01-26 03:58:40 +0900

    Fix: 에러 코드 수정

--- src/errors/session.error.js
+++ src/errors/session.error.js
@@ -3,26 +3,21 @@ import { BadRequestError, NotFoundError, InternalServerError } from "./base.erro
 /**
  * 400: 올바르지 않은 Arguments 입력 (SESSION_400_01)
  */
-export class UnExpectArgumentsError extends BadRequestError {
-  constructor(
-    code = "SESSION_400_01",
-    message = "올바르지 않은 Arguments 입력입니다.",
-    data = null
-  ) {
+export class SelfFriendRequestError extends BadRequestError {
+  constructor(code = "FRIEND_SELFREQUEST_ERROR", message = "자기 자신과는 친구 신청을 할 수 없습니다.", data = null) {
     super(code, message, data);
   }
 }
 
-/**
- * 404: 존재하지 않는 세션 참가자 (SESSION_404_01)
- */
+export class UnExpectArgumentsError extends BadRequestError {
+    constructor(code = "SESSION_UNEXPECTEDARGS_ERROR", message = "올바르지 않은 Arguments 입력입니다.", data = null) {
+        super(code, message, data);
+    }
+}
+
 export class SessionParticipantNotFoundError extends NotFoundError {
-  constructor(
-    code = "SESSION_404_01",
-    message = "존재하지 않는 세션 참가자입니다.",
-    data = null
-  ) {
-    super(code, message, data);
+  constructor(code = "SESSION_PARTICIPANTNOTFOUND_ERROR", message = "존재하지 않는 세션 참가자입니다.", data = null) {
+        super(code, message, data);
   }
 }
 
@@ -30,25 +25,14 @@ export class SessionParticipantNotFoundError extends NotFoundError {
  * 500: 세션 처리 중 서버 오류 (SESSION_500_01)
  */
 export class SessionInternalError extends InternalServerError {
-  constructor(
-    code = "SESSION_500_01",
-    message = "세션 처리 중 서버 오류가 발생했습니다.",
-    data = null
-  ) {
-    super(code, message, data);
+  constructor(code = "SESSION_500", message = "친구 처리 중 서버 오류가 발생했습니다.", data = null) {
+        super(code, message, data);
   }
 }
 
-/**
- * 500: 편지 주고 받은 횟수 초과 (SESSION_500_02)
- */
-export class MaxTurnIsOver extends InternalServerError {
-  constructor(
-    code = "SESSION_500_02",
-    message = "편지 주고 받은 횟수가 10번이 되었습니다.",
-    data = null
-  ) {
-    super(code, message, data);
+export class MaxTurnIsOver extends BadRequestError {
+  constructor(code = "SESSION_TURNOVER_ERROR", message = "편지 주고 받은 횟수가 10번이 되었습니다.", data = null) {
+        super(code, message, data);
   }
 }
 
@@ -56,12 +40,8 @@ export class MaxTurnIsOver extends InternalServerError {
  * 404: 세션을 찾을 수 없음 (SESSION_404_02)
  */
 export class SessionNotFoundError extends NotFoundError {
-  constructor(
-    code = "SESSION_404_02",
-    message = "세션을 찾을 수 없습니다.",
-    data = null
-  ) {
-    super(code, message, data);
+  constructor(code = "SESSION_NOTFOUND_ERROR", message = "세션을 찾을 수 없습니다.", data = null) {
+        super(code, message, data);
   }
 }
 
@@ -69,17 +49,13 @@ export class SessionNotFoundError extends NotFoundError {
  * 500: 세션 개수 초과 (SESSION_500_03)
  */
 export class SessionCountOverError extends InternalServerError {
-  constructor(
-    code = "SESSION_500_03",
-    message = "세션이 10개 이상입니다.",
-    data = null
-  ) {
-    super(code, message, data);
+  constructor(code = "SESSION_COUNTOVER_ERROR", message = "세션이 10개 이상입니다.", data = null) {
+        super(code, message, data);
   }
 }
 
 export class SessionFullError extends NotFoundError {
-  constructor(data = null, message = "세션을 맺을 유저가 없습니다.") {
-    super(message, 404, "SESSION_FULL", data);
+  constructor(code = "SESSION_FULL_ERROR", message = "세션을 맺을 유저가 없습니다.", data = null) {
+        super(code, message, data);
   }
 }
