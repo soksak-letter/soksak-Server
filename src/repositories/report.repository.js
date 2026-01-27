3e8354f4f96e73e632066c1b39f6486c0a991599
user040131 <seungjub270@gmail.com>
2026-01-27 09:12:51 +0900

    Feat: 신고 상세 조회 api

--- src/repositories/report.repository.js
+++ src/repositories/report.repository.js
@@ -1,7 +1,7 @@
 import { prisma } from "../db.config.js";
 import { xprisma } from "../xprisma.js";
 
-export const insertUserReport = async(reporterUserId, targetUserId, letterId) => {  
+export const insertUserReport = async (reporterUserId, targetUserId, letterId) => {
     const result = await prisma.userReport.create({
         data: {
             reporterUserId: reporterUserId,
@@ -15,26 +15,25 @@ export const insertUserReport = async(reporterUserId, targetUserId, letterId) =>
 }
 
 export const insertUserReportReason = async (reportId, reasons) => {
-  // reasons: string[]
-  if (!Array.isArray(reasons) || reasons.length === 0) {
-    return { count: 0 }; // 또는 throw
-  }
+    if (!Array.isArray(reasons) || reasons.length === 0) {
+        return { count: 0 }; // 또는 throw
+    }
 
-  return await prisma.userReportReason.createMany({
-    data: reasons.map((reason) => ({
-      reportId,
-      reason, // ✅ 한 행당 reason은 문자열 1개
-    })),
-    // skipDuplicates: true, // (선택) unique 제약이 있을 때 중복 무시
-  });
+    return await prisma.userReportReason.createMany({
+        data: reasons.map((reason) => ({
+            reportId,
+            reason, 
+        })),
+    });
 };
 
-export const getUserReportAndReasonByUserId = async(reporterUserId) => {
+export const getUserReportAndReasonByUserId = async (reporterUserId) => {
     const result = await prisma.userReport.findMany({
         where: {
             reporterUserId
         },
         select: {
+            id: true,
             createdAt: true,
             status: true,
             reasons: {
@@ -43,4 +42,24 @@ export const getUserReportAndReasonByUserId = async(reporterUserId) => {
         }
     })
     return result;
-}   
+}
+
+export const getUserReportAndReasonByReportId = async (reporterUserId, id) => {
+    return await prisma.userReport.findFirst({
+        where: {
+            id,
+            reporterUserId
+        },
+        select: {
+            id: true,
+            reporterUserId: true,
+            targetUserId: true,
+            letterId: true,
+            status: true,
+            createdAt: true,
+            reasons: {
+                select: { reason: true }
+            }
+        }
+    })
+}
