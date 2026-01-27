54a6d65964a1018f64e47ddcc64787d985600893
seungjub270 <seungjub270@gmail.com>
2026-01-27 12:38:30 +0900

    Fix: weeklyReport.controller.js의 try 수정

--- src/controllers/weeklyReport.controller.js
+++ src/controllers/weeklyReport.controller.js
@@ -3,6 +3,7 @@ import { readWeeklyReport } from "../services/weeklyReport.service.js";
 export const handleGetWeeklyReport = async (req, res, next) => {
   const userId = req.user.id;
   const result = await readWeeklyReport(userId);
+  try{
   res
     .status(200)
     .success({ message: "주간 리포트 조회 성공하였습니다.", result });
