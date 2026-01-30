44154159a2308833027126a47b26070a82c3688c
pumisj <pumisj@naver.com>
2026-01-31 01:34:46 +0900

    feat: 홈 화면 유저 정보 응답 추가 (#144)

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,16 +1,22 @@
 import { getUserLetterStats } from "../services/letter.service.js";
 import { getTodayQuestion } from "../services/question.service.js";
 import { countUserSession } from "../services/session.service.js";
+import { getMyProfile } from "../services/user.service.js";
 
 export const HandleGetHomeDashboard = async (req, res, next) => {
     const userId = req.user.id;
 
     try{
+        const {nickname, profileImageUrl} = await getMyProfile({userId});
         const todayQuestion = await getTodayQuestion();
         const letterStats = await getUserLetterStats(userId);
         const sessionCount = await countUserSession(userId);
 
         res.status(200).success({
+            user: {
+                nickname,
+                profileImageUrl
+            },
             todayQuestion, 
             letterStats,
             sessionCount
