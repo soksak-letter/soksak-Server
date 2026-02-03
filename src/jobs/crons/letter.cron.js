0714669e6edc0aabe49d3b48928f7c51eeafefb3
pumisj <pumisj@naver.com>
2026-02-04 01:06:08 +0900

    fix: 예약퇸 편지 전송 cron 시간 설정 변경 (#152)

--- src/jobs/crons/letter.cron.js
+++ src/jobs/crons/letter.cron.js
@@ -3,7 +3,7 @@ import { sendScheduledLetters } from "../bootstraps/letter.bootstrap.js";
 
 export const sendScheduledLettersCron = () => {
     cron.schedule(
-        '* * * * *', 
+        '0 0 * * *', 
         async () => {
             console.log("편지 전송을 시작합니다.");
             await sendScheduledLetters();
